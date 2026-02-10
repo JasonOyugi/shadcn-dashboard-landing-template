20c4744bd03471765dc5371c82726078ab70abc3
JasonOyugi <Jason.oyugi@gatsbyafrica.org.uk>
2026-02-10 07:14:16 +0300

    Remove StatsSection from landing page
    Removed StatsSection from the landing page.

--- vite-version/src/app/landing/page.tsx
+++ vite-version/src/app/landing/page.tsx
@@ -4,7 +4,6 @@ import React from 'react'
 import { LandingNavbar } from './components/navbar'
 import { HeroSection } from './components/hero-section'
 import { LogoCarousel } from './components/logo-carousel'
-import { StatsSection } from './components/stats-section'
 import { FeaturesSection } from './components/features-section'
 import { TeamSection } from './components/team-section'
 import { TestimonialsSection } from './components/testimonials-section'
@@ -29,7 +28,6 @@ export default function LandingPage() {
       <main>
         <HeroSection />
         <LogoCarousel />
-        <StatsSection />
         <AboutSection />
         <FeaturesSection />
         <TeamSection />
